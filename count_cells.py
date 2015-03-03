8285f79bb4d970244f9488c9956fcf4fbc5df6d1
Higgens1 <h1ggens@arcor.de>
2015-03-03 17:23:19 +0100

    second commit
    hello

--- count_cells.py
+++ count_cells.py
@@ -9,6 +9,7 @@ import pylab
 import mahotas as mh
 import matplotlib.pyplot as plt
 
+
 # helper functions
 # array loop
 def closing_element(element_size):
@@ -31,7 +32,7 @@ def closing_element(element_size):
     return element
 
 # load img
-working_dir = '/Users/alexander_neumann/Desktop/bioinf/steady_state/131015_zoom1.7/'
+working_dir = "C:/project_image_processing/steady_state/131015_zoom1.7/"
 img_dir = working_dir + 'Series006_z0_ch00.tif'
 img = mh.imread(img_dir)
 
@@ -49,11 +50,30 @@ img_distance = mh.distance(img_cut)
 closing_size = closing_element(9)
 img_closed = mh.close(img_distance > 5, Bc=closing_size)
 
+# skeleton transform
+img_dist = mh.distance(img_closed)
+img_dist = mh.gaussian_filter(img_dist, 8)
+img_dist = img_dist.astype(int)
+
+labels_dist, nr_objects = mh.label(img_dist)
+
+centers = mh.center_of_mass(img_dist, labels_dist)
+
+print type(centers)
+# find max values, seeds
+rmax = mh.regmax(img_dist)
+
 # show original vs processed
 img_processed = img_closed
+
 pylab.gray()
-plt.subplot(1, 2, 1)
+plt.subplot(2, 2, 1)
 plt.imshow(img)
-plt.subplot(1, 2, 2)
+plt.subplot(2, 2, 2)
 plt.imshow(img_processed)
+plt.subplot(2, 2, 3)
+plt.imshow(img_dist)
+plt.subplot(2, 2, 4)
+#pylab.imshow(mh.overlay(img_dist, labels_dist))
+pylab.imshow(centers)
 pylab.show()
